530df040260478264a18743d5af7b7e53e247936
Nour Saleh Rabee <rabee.nour123@gmail.com>
2024-10-04 20:10:54 +0300

    Singleton Thread safe Doubled Checked

--- singleton/src/MemoryLogger.java
+++ singleton/src/MemoryLogger.java
@@ -12,21 +12,24 @@ public class MemoryLogger {
     private static MemoryLogger instance = null;
     private static final Object lock = new Object();
 
-    private MemoryLogger(){
+    private MemoryLogger() {
 
     }
 
     public static MemoryLogger getLogger() {
         // In Java, the synchronized keyword is used to ensure that only one thread can access a particular block of code at a time.
 
-        synchronized (lock) {
+        if (instance == null) {
 
-            if (instance == null) {
-                instance = new MemoryLogger();
+            synchronized (lock) {
+
+                if (instance == null) {
+                    instance = new MemoryLogger();
+                }
             }
+        }
         return instance;
 
-        }
     }
 
     public List<LogMessage> getLogs() {
